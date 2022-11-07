a2f2bf2c4945950cd523afff2fbcfb87101cafd4
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-11-07 14:18:38 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const baseUrl = "http://localhost:8000"
+
+  const [categories, SetCategories] = useState([])
+
+  const titleRef = useRef()
+  const descriptionRef = useRef()
+  const categoryRef = useRef()
+
+  useEffect(() => {
+    fetch(`${baseUrl}/categories`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+      })
+  }, [])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name='title' placeholder='Add new todo..' />
+        <br />
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <select name="category_id" id="category_id">
+          {categories.map((category) => }
+          {/* {categories.map((category) => (
+            <option key={category.id}>{category}</option>
+          )} */}
+        </select>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
